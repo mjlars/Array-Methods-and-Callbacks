f72821eeecb0919bd7b05a27aee3496cd33c4432
Matthew Larson <maflarson@gmail.com>
2020-11-10 19:40:45 -0700

    d

--- index.js
+++ index.js
@@ -70,14 +70,14 @@ Parameters:
  */
 
 function getWinnersByYear() {
-    /* code here */
+    
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
 function getAverageGoals() {
-   /* code here */
+   
 }
 
 
